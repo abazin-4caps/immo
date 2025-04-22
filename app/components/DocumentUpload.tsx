1696f9fd73c3cd91dfe4724008a68f0e36ab73f1
Alexandre BAZIN <alexandre.bazin@4-caps.com>
2025-04-22 19:55:35 +0200

    fix: utilisation d'unpkg avec version fixe pour le PDF worker

--- app/components/DocumentUpload.tsx
+++ app/components/DocumentUpload.tsx
@@ -9,7 +9,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Configuration de worker pour react-pdf
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@5.1.91/build/pdf.worker.min.js';
 
 interface Document {
   id: string;
