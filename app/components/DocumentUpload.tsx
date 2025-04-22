cc7065a015114106e4bb44cd7fcabbcabb9719c9
Alexandre BAZIN <alexandre.bazin@4-caps.com>
2025-04-22 18:36:48 +0200

    fix: correction du chemin du PDF worker

--- app/components/DocumentUpload.tsx
+++ app/components/DocumentUpload.tsx
@@ -10,7 +10,7 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Configuration de worker pour react-pdf
 if (typeof window !== 'undefined') {
-  pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.min.js';
+  pdfjs.GlobalWorkerOptions.workerSrc = `${window.location.origin}/pdf.worker.min.mjs`;
 }
 
 interface Document {
