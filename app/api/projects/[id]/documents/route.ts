ec2e2d810e6658bec249a7af7420157bfe800d53
Alexandre BAZIN <alexandre.bazin@4-caps.com>
2025-04-22 20:55:25 +0200

    fix: utilisation de CLOUDINARY_CLOUD_NAME au lieu de NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME

--- app/api/projects/[id]/documents/route.ts
+++ app/api/projects/[id]/documents/route.ts
@@ -6,7 +6,7 @@ import prisma from '@/lib/prisma';
 
 // Configuration de Cloudinary
 cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
